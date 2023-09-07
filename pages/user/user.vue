2260df3aa620869774fc818ea1f24e4eb91dbb33
Unknown <1016683587@qq.com>
2023-09-07 23:19:56 +0800

    init

---
+++ pages/user/user.vue
@@ -0,0 +1,13 @@
+<template>
+	<view class="page flex-center">
+	</view>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style>
+	.page {
+		min-height: calc(100vh - var(--window-bottom));
+	}
+</style>
